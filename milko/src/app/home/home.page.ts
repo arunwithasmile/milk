3e96cd812f0a8f5d93063975c77c513eb33065aa
Arun S P <arun@Aruns-MacBook-Pro.local>
2019-12-05 13:05:26 +0530

    Using loop

--- milko/src/app/home/home.page.ts
+++ milko/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  total = 0;
 
+  items = [{
+  	name: "Milk",
+  	qty: "500 ml",
+  	price: 60,
+  	desc: "Fresh Milk from A2 cows",
+  	imgUrl: "milk.jpg"
+  }, {
+  	name: "Curd",
+  	qty: "500 ml",
+  	price: 70,
+  	desc: "Freshly prepared from Organic A2 Milk.",
+  	imgUrl: "curd.jpg"
+  }, {
+  	name: "Ghee",
+  	qty: "500 gms",
+  	price: 250,
+  	desc: "Prepared from fresh Organic A2 Milk. No preservatives added.",
+  	imgUrl: "ghee.jpg"
+  }];
+
+  constructor(private router: Router) {
+  	//this.router.navigateByUrl('/login');
+  }
+
+  addToCart(item){
+  	this.total += item.price;
+  	console.log("Total :" + this.total);
+  }
 }
