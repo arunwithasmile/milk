50abc50e0548732ff69cd6c2ebe1d7d57561036b
Arun S P <arun@Aruns-MacBook-Pro.local>
2019-12-07 22:52:24 +0530

    Don't want iOS as default

--- milko/src/app/app.module.ts
+++ milko/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot({
-            mode: 'ios'
+            //mode: 'ios'
         }), AppRoutingModule],
   providers: [
     StatusBar,
